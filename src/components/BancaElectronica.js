0009e8d95693b2d404ed00d984d459b613d40601
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-06-25 21:33:59 -0400

    formulario de banca

---
+++ src/components/BancaElectronica.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class BancaElectronica extends React.Component{
+    render(){
+        return(
+            <div clas="container">
+              <div class="row">
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col"></div>
+              </div>
+              <div class="row">
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col"></div>
+              </div>
+              <div class="row">
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col"></div>
+              </div>
+                    
+             </div>
+        )
+    }
+
+
+
+}
