c883595ffe2cf9d518d832ddbf1e97824e655bea
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-06-25 21:34:12 -0400

    Update Pagar.js

--- src/pages/Pagar.js
+++ src/pages/Pagar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import useGlobal from "../hooks/useGlobal";
-
+import FormCreditCard from "../components/FormCreditCard";
 export default function Pagar() {
   const { userDef, setUserDef } = useGlobal();
 
   return (
-    <div>
+    <div class="container">
       <h1>Pagar</h1>
+      
+        <FormCreditCard/>
+      
     </div>
   );
 }
