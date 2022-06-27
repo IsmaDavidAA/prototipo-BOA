531e1b09ed6232175d6d6e48b9bf53444f45e8a7
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-06-26 21:34:06 -0400

    formmovil

--- src/pages/Pagar.js
+++ src/pages/Pagar.js
@@ -2,15 +2,17 @@ import React from "react";
 import useGlobal from "../hooks/useGlobal";
 import FormCreditCard from "../components/FormCreditCard";
 import BancaElectronica from "../components/BancaElectronica";
+import FormMovil from "../components/FormMovil";
 export default function Pagar() {
   const { userDef, setUserDef } = useGlobal();
 
   return (
     <div class="container">
       <h1>Pagar</h1>
-      
+        <></>
         <FormCreditCard/>
         <BancaElectronica/>
+        <FormMovil/>
       
     </div>
   );
