8f02a0030ee6ddc397470f6a3a4ce3a2a13b9e34
Favious <faviopaco@gmail.com>
2022-06-30 13:49:49 -0400

    fix spaces between options

--- src/pages/ServicioEspecial.js
+++ src/pages/ServicioEspecial.js
@@ -9,12 +9,13 @@ export default function ServicioEspecial() {
   const { userDef, setUserDef } = useGlobal();
 
   return (
-    <Container>
-      <Row as={Col} className="text-center">
+    <Container className="mt-5">
+      <Row as={Col} className="text-center ">
         <h1>Servicios especiales(Opcional)</h1>
         <ExtraLuggage />
         <CarRental />
         <Insurance />
+        <Button className="fs-3 col-4 mx-auto mt-5">Continuar</Button>
       </Row>
     </Container>
   );
