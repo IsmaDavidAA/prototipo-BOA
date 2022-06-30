fe3187bb6c891c07858a8e7acb1d90726074e33e
Favious <faviopaco@gmail.com>
2022-06-30 12:09:03 -0400

    add extra luggage component

--- src/pages/ServicioEspecial.js
+++ src/pages/ServicioEspecial.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import useGlobal from "../hooks/useGlobal";
+import ExtraLuggage from "../components/ExtraLuggage";
 
 export default function ServicioEspecial() {
   const { userDef, setUserDef } = useGlobal();
 
   return (
-    <div>
-      <h1>ServicioEspecial</h1>
-    </div>
+    <Container>
+      <Row as={Col} className="text-center">
+        <h1>Servicios especiales</h1>
+        <ExtraLuggage />
+      </Row>
+    </Container>
   );
 }
