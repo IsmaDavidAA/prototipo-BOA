25f15569ee70867b31fb2e3769f9e4d57475328a
IsmaDavidAA <idaasannmvgd@gmail.com>
2022-07-03 14:49:38 -0400

    add header

--- src/styles/StyledComponents.js
+++ src/styles/StyledComponents.js
@@ -44,3 +44,34 @@ export const ContainerJoin = styled.div`
   padding: 15px;
   width: 100%;
 `;
+
+export const HeaderStyled = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  margin: 0 auto;
+  width: 100%;
+  padding: 10px;
+  background-color: #d8d8d8;
+`;
+
+export const NavStyled = styled.nav`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: flex-start;
+  margin: 0 auto;
+  width: 100%;
+  padding: 10px;
+`;
+
+export const ContainerOption = styled.button`
+  display: flex;
+  color: #253a57;
+  background-color: #d8d8d8;
+  padding: 10px;
+  border: none;
+  size: 1.8rem;
+  font-weight: bold;
+`;
