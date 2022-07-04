615ae46b10dcb21037f4427a6383627947f54c5c
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-07-04 12:42:44 -0300

    Formularios con formik

--- src/components/FormCreditCard.js
+++ src/components/FormCreditCard.js
@@ -1,47 +1,33 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form } from "formik";
-import { Button } from "bootstrap";
-export default class FormCreditCard extends React.Component{
-    state = {
-        cvv: '',
-        expiry: '',
-        focus: '',
-        nombre: '',
-        number: '',
-      };
-     
-      handleInputFocus = (e) => {
-        this.setState({ focus: e.target.name });
-      }
+import { Form,Field,withFormik, ErrorMessage } from "formik";
+
+
+const FormCreditCard = (props)=>{
+    const{ isSubmitting,isValid}=props;
       
-      handleInputChange = (e) => {
-        const { name, value } = e.target;
-        
-        this.setState({ [name]: value });
-      } 
-      
-render(){
+
     return(
         <div class="card p-2 m-3 border-0" >
             <div class="card-body ">
-            <form >
+            <Form >
                 <div class="form-group ">
-                    <input
+                    <Field
                     class="form-control"
                     type="text"
                     name="number"
                     pattern="[0-9]{0,16}"
                     placeholder="Número de tarjeta de credito"
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
-                    ></input>
+                    ></Field>
+                    <ErrorMessage name="number" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>
                 </div>
                 
                 <div  class="row" >
                     <div class="col-md">
-                        <label for="mes">Vencimiento</label>
-                        <select name="mes"  class="form-control">
+                        <label for="mes"><b>Vencimiento</b></label>
+                        <Field as="select" name="mes"  class="form-control">
                             <option value="1">01</option>
                             <option value="2">02</option>
                             <option value="3">03</option>
@@ -54,73 +40,133 @@ render(){
                             <option value="10">10</option>
                             <option value="11">11</option>
                             <option value="12">12</option>
-                        </select>
-                       
+                        </Field>
+                        
                     </div>
                     <div class="col-md" >
                          <label for="año"> </label>
-                        <select name="año" class="form-control" >
+                        <Field as="select" name="año" class="form-control" >
                                 <option value="22">2022</option>
                                 <option value="23">2023</option>
                                 <option value="24">2024</option>
                                 <option value="25">2025</option>
                                 <option value="26">2026</option>
 
-                            </select>
+                            </Field>
                     </div>
                    
                     <div class="col-md"  >
-                        <label for="cvv "> CVV
+                        <label for="cvv"> <b>CVV</b> 
                         </label>
-                        <input
+                        <Field
                         name="cvv"
                         type="text"
                         maxLength="4"
                         class="form-control"
-                        onChange={this.handleInputChange}
-                        onFocus={this.handleInputFocus}
+
                         
                         >
                             
-                        </input>
-                    
+                        </Field>
+                        <ErrorMessage name="cvv" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>
                     </div>
                     
                 </div>
                 <divi class="form-group " >
-                    <label for="Nombre">Nombre</label>
-                    <input
+                    <label for="nombre"><b>Nombre</b></label>
+                    <Field
                     type="text"
-                    name="Nombre"
+                    name="nombre"
                     class="form-control"
                     placeholder=""
-                    onChange={this.handleInputChange}
+
                     >
                     
-                </input>
+                </Field>
+                <ErrorMessage name="nombre" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>
                 </divi>
-                <divi class="form-group ">
-                    <label for="Apellido">Apellido</label>
-                    <input
+                <divi class="form-group p-2 ">
+                    <label for="apellido"><b>Apellido</b></label>
+                    <Field
                     type="text"
-                    name="Apellido"
+                    name="apellido"
                     class="form-control"
                     placeholder=""
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
+
                     >
-                </input>
+                </Field>
+                <ErrorMessage name="apellido" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>
                 </divi>
-                
-           
-                
-                
-            </form>   
+                <div class="d-flex justify-content-center p-2-auto">
+                <button type="submit" class="btn btn-primary btn-lg btn-block submit ${isSubmitting || !isValid ? 'disabled' : ''} " disabled={isSubmitting || !isValid} >Pagar</button>
             </div>
-            <div class="mx-auto">
-                <button type="button" class="btn btn-primary btn-lg btn-block ">Pagar</button>
+            </Form>   
             </div>
+            
         </div>
     )
-}    
+   
 }
+export default withFormik({
+    mapPropsToValues(props){
+        return{
+            number: "",
+            mes: "1",
+            año: "22",
+            cvv: "",
+            nombre: "",
+            apellido:""
+
+        }
+    },validate(values){
+        const errors = {}
+        
+        if(!values.number){
+            errors.number ="El  numero de tarjeta es necesario"
+            
+        }else if(values.number.length < 15 ||  values.number.length > 16){
+            errors.number ="Ingrese un numero de tarjeta valido"
+   
+        }if(!(/^([0-9])*$/.test(values.number))){
+            errors.number = "Ingrese un numero de tarjeta valido"
+        }
+        if(!values.nombre){
+            errors.nombre ="El nombre del  propietario de la tajeta  es necesario"
+        }else if(!(/^[a-z ñ.áéíóúäëïöü\'-]*$/.test(values.nombre))){
+            errors.nombre = "Ingrese un nombre  valido"
+        }else if(values.nombre.length < 3){
+            errors.nombre="Ingrese un nombre  valido"
+        }
+        if(!values.cvv){
+            errors.cvv ="El  numero cvv de la terjeta es necesario"
+            
+        }else if(values.cvv.length < 3){
+            errors.cvv ="Ingrese un numero de cvv  valido"
+   
+        }if(!(/^([0-9])*$/.test(values.cvv))){
+            errors.cvv = "Ingrese un numero de cvv  valido"
+        }
+        if(!values.apellido){
+            errors.apellido ="El apellido del  propietario de la tajeta  es necesario"
+        }else if(!(/^[a-z ñ.áéíóúäëïöü\'-]*$/.test(values.apellido))){
+            errors.apellido = "Ingrese un apellido  valido"
+        }
+    return errors
+    
+    },
+    handleSubmit(values,formikBag){
+       
+        console.log(values);
+        formikBag.setSubmitting(false)
+       return(
+        alert("Tu pago se esta procesando, gracias por utilizar los servicios!!!")
+       )
+        
+    }
+}) (FormCreditCard)
--- src/components/FormMovil.js
+++ src/components/FormMovil.js
@@ -1,77 +1,147 @@
 
-import React ,{useState}from "react";
+import React from "react";
 import captcha from "../img/captcha.jpg"
+import { withFormik, Field, ErrorMessage, Form } from "formik";
 
-const  FormMovil = ()=>{
 
+const  FormMovil = (props)=>{
+    const{ isSubmitting,isValid,modal}=props;
+   
     return(
         <div class="card p-2 m-3 border-0 ">
             <div class="card-body">
-                <form class="pr-3">
-                    <div clas="form-group">
-                        <label for="Linea"> Linea</label>
-                        <input 
-                        name="Linea"
+                <Form class="pr-3" >
+                    <div clas="form-group m-2">
+                        <label for="linea"> <b>linea</b></label>
+                        <Field 
+                        name="linea"
                         type = "text"
                         class = "form-control"
                         placeholder = "Numero de la linea"
 
-                        >
-                        
-                        </input>
-                    
+                        />
+                      <ErrorMessage name="linea" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>
                     </div>
-                    <div clas="form-group">
-                        <label for="nombre"> Nombre Completo</label>
-                        <input 
+                   
+                    <div clas="form-group m-2">
+                        <label for="nombre"> <b>Nombre Completo</b></label>
+                        <Field 
                         name="nombre"
                         type = "text"
                         class = "form-control"
                         placeholder = "Nombre del abonado"
 
-                        >
-                        </input>                
+                        />
+                        <ErrorMessage name="nombre" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>         
                     </div>
-                    <div clas="form-group">
-                        <label for="numero"> Nro. Documento</label>
-                        <input 
+                    <div clas="form-group m-2">
+                        <label for="numero"> <b>Nro. Documento</b></label>
+                        <Field 
                         name="numero"
                         type = "text"
                         class = "form-control"
                         placeholder = "Número del abonado"
                     
-                        >
-                        </input>                
+                        />
+                          <ErrorMessage name="numero" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>           
                     </div>
                     <div class="row"> 
-                        <div clas="col col-lg-2 form-group">
-                            <p > Ingrese los caracteres que aparecen en la imagen:</p>
+                        <div clas="col col-lg-2 form-group m-2">
+                            <b> Ingrese los caracteres que aparecen en la imagen:</b>
                             <div class="d-flex justify-content-center">
                             <img src= {captcha} clas ="img-fluid mx-auto " ></img>
                             </div>
                             <label for="capt"> </label>
-                            <input 
+                            <Field 
                             name="capt"
                             type = "text"
                             class = "form-control"
                             placeholder = ""
                       
-                            >
-                            </input>                
+                            />
+                              <ErrorMessage name="capt" >
+                        {message => <div class="alert alert-warning">{message}</div>}
+                      </ErrorMessage>              
                         </div>
 
                     </div>
                     
                     <div class="d-flex justify-content-center p-2">
-                            <button type="button" class="btn btn-primary btn-lg btn-block  ">Pagar</button>
+                            <button type="submit" class="btn btn-primary btn-lg btn-block submit ${isSubmitting || !isValid ? 'disabled' : ''} " disabled={isSubmitting || !isValid} >Pagar</button>
                      </div>
-                </form>
-                
+                </Form>
+              
             </div>
         
         
         </div>
-
+        
     )
 }
-export default FormMovil
+export default  withFormik({
+    mapPropsToValues(props){
+        return{
+            linea: "",
+            nombre: "",
+            numero: "",
+            capt: ""
+
+        }
+    },
+    validate(values){
+        const errors = {}
+        
+        if(!values.linea){
+            errors.linea ="El  numero de linea es necesario"
+            
+        }else if(values.linea.length < 8 ||  values.linea.length > 8){
+            errors.linea ="La linea debe tener 8 numeros"
+   
+        }if(!(/^([0-9])*$/.test(values.linea))){
+            errors.linea = "La linea solo puede contener numeros"
+        }
+        if(!values.nombre){
+            errors.nombre ="El nombre del abonado es necesario"
+        }else if(!(/^[a-z ñ.áéíóúäëïöü\'-]*$/.test(values.nombre))){
+            errors.nombre = "Ingrese un nombre de abonado valido"
+        }else if(values.nombre.length < 5){
+            errors.nombre="Ingrese un nombre de abonado valido"
+        }
+        if(!values.numero){
+            errors.numero ="El  numero de documento es necesario"
+            
+        }else if(values.numero.length < 7 ){
+            errors.numero ="Ingrese un numero de carnet valido"
+   
+        }
+        if(!(/^([0-9])*$/.test(values.numero))){
+            errors.numero = "Ingrese un numero de carnet valido"
+        }
+        if(!values.capt){
+            errors.capt ="Por favro ingrese las letras de la imagen"
+            
+        }else if(values.capt != "qGphJD"){
+            
+            errors.capt ="Error en captcha"
+            
+        }
+    
+    return errors
+    
+    },
+    handleSubmit(values,formikBag){
+       
+        console.log(values);
+        formikBag.setSubmitting(false)
+       return(
+        alert("Tu pago se esta procesando, gracias por utilizar los servicios!!!")
+       )
+        
+    }
+}) (FormMovil)
